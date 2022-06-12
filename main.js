81e3dd519878d27c34f203e44d2e4ac814aacf85
UlanTol <104356514+UlanTol@users.noreply.github.com>
2022-06-12 21:08:07 +0600

    hey

---
+++ main.js
@@ -0,0 +1,99 @@
+
+let elevator = {
+  min: 1,
+  max: 5,
+  currentFloor: 1,
+  printFloor: function () {
+    console.log(this.currentFloor + "floor");
+  },
+  printError: function () {
+    console.log("error");
+  },
+  upOneFloor: function (type, margin) {
+    if (this.currentFloor >= this.min && this.currentFloor < this.max) {
+      this.currentFloor++;
+      let marginBottom = parseInt(getComputedStyle(elev).marginBottom);
+      elev.style.marginBottom = `${
+        type === "up" ? marginBottom + 100 : margin
+      }px`;
+      elev.style.transition = "1s";
+      this.printFloor();
+    } else { 
+      this.printError();
+    }
+    if (margin > 500) {
+      elev.style.marginBottom = `${margin}px`;
+    }
+  },
+  downOneFloor: function (type, margin) {
+    if (this.currentFloor > this.min && this.currentFloor <= this.max) {
+      this.currentFloor--;
+      let marginBottom = parseInt(getComputedStyle(elev).marginBottom);
+      elev.style.marginBottom = `${
+        type === "down" ? marginBottom - 100 : margin
+      }px`;
+      elev.style.transition = "1s";
+      this.printFloor();
+    } else {
+      this.printError();
+    }
+  },
+  toFloor: function (floor, margin) {
+    if (floor >= this.min && floor <= this.max) {
+      while (this.currentFloor < floor) {
+        this.upOneFloor("floor", margin);
+      }
+      while (this.currentFloor > floor) {
+        this.downOneFloor("floor", margin);
+      }
+    } else {
+      this.printError();
+    }
+  },
+};
+let floorOne = document.getElementById("floor-1");
+let floorTwo = document.getElementById("floor-2");
+let floorThree = document.getElementById("floor-3");
+let floorFour = document.getElementById("floor-4");
+let floorFive = document.getElementById("floor-5");
+
+let elev = document.getElementById("elevator");
+let btnUp = document.createElement("button");
+btnUp.setAttribute("id", "btn-up");
+btnUp.style.position = 'absolute'
+btnUp.style.marginTop = '350px'
+btnUp.style.marginLeft = '160px'
+btnUp.innerHTML = "UP";
+document.body.append(btnUp);
+btnUp.addEventListener("click", function () {
+  elevator.upOneFloor("up", 100);
+});
+
+let btnDown = document.createElement("button");
+btnDown.setAttribute("id", "btn-down");
+btnDown.style.position = 'absolute'
+btnDown.style.marginTop = '380px'
+btnDown.style.marginLeft = '160px'
+btnDown.innerHTML = "DOWN";
+document.body.append(btnDown);
+btnDown.addEventListener("click", function () {
+  elevator.downOneFloor("down", 100);
+});
+
+
+floorOne.addEventListener("click", function () {
+  elevator.toFloor(1, 0);
+});
+floorTwo.addEventListener("click", function () {
+  elevator.toFloor(2, 100);
+});
+floorThree.addEventListener("click", function () {
+  elevator.toFloor(3, 200);
+});
+floorFour.addEventListener("click", function () {
+  elevator.toFloor(4, 300);
+});
+floorFive.addEventListener("click", function () {
+  elevator.toFloor(5, 400);
+});
+
